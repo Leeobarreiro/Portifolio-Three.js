aa1405de53ce0a2c1c7788e8bc72f81c1956221a
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2023-12-21 13:46:53 -0300

    scroll add

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -146,16 +146,48 @@ const languages = [
 const SkillsSection = () =>{
 return(
     <Section>
-        <div>
+        <motion.div whileInView={"visible"}>
             <h2 className="text-5xl font-bold">Skills</h2>
             <div className=" mt-8 space-y-4">
                 {skills.map((skill, index) => (
                     <div className="w-64" key={index}>
-                      <h3 className="text-xl font-bold text-gray-800">{skill.title}</h3> 
+                      <motion.h3 className="text-xl font-bold text-gray-800"
+                        initial={{
+                            opacity: 0,
+                        }}
+                        variants={{
+                            visible:{
+                                opacity: 1,
+                                transition:{
+                                    duration: 1,
+                                    delay: 1 + index * 0.2,
+                                }
+                            }
+
+                        }}
+                       
+                      
+                      >{skill.title}</motion.h3> 
                       <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                        <div 
+                        <motion.div 
                         className="h-full bg-indigo-500 rounded-full"
-                        style={{ width: `${skill.level}%`}} />
+                        style={{ width: `${skill.level}%`}} 
+                        initial={{
+                            scaleX: 0,
+                            originX: 0,
+                        }}
+                        variants={{
+                            visible:{
+                            scaleX: 1,
+                            transition:{
+                                duration: 1,
+                                delay: 1 + index * 0.2,
+                            }
+                        }
+                        }}
+                        
+                        
+                        />
                         </div>
                         </div>
                 ))}
@@ -164,24 +196,56 @@ return(
             <h2 className="text-5xl font-bold mt-10">Linguagens</h2>
             <div className=" mt-8 space-y-4">
                 {languages.map((lng, index) => (
-                <div className="w-64" key={index}>
-                    <h3 className="text-xl font-bold text-gray-800">{lng.title}</h3>
-                    <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                        <div 
-                        className="h-full bg-indigo-500 rounded-full"
-                        style={{ width: `${lng.level}%` }} />
-            </div>
-            </div>             
+            <div className="w-64" key={index}>
+        <motion.h3 className="text-xl font-bold text-gray-800"
+                    initial={{
+                    opacity: 0,
+                    }}
+                    variants={{
+                    visible:{
+                    opacity: 1,
+                    transition:{
+                    duration: 1,
+                    delay: 2 + index * 0.2,
+                    }
+                }
+
+               }}
+  
+ 
+                >{lng.title}</motion.h3> 
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+        <motion.div 
+                className="h-full bg-indigo-500 rounded-full"
+                style={{ width: `${lng.level}%`}} 
+                initial={{
+                scaleX: 0,
+                 originX: 0,
+                }}
+                 variants={{
+                 visible:{
+                  scaleX: 1,
+                 transition:{
+                duration: 1,
+                  delay: 2 + index * 0.2,
+                }
+            }
+        }}
+   
+   
+                 />
+                </div>
+                </div>            
                 ))}
             </div>
             </div>
-            </div>
+            </motion.div>
             
                 
 
-    </Section>
-);
-};
+            </Section>
+            );
+        };
 
 const ContactSection = () => {
 
