e639b9db2f9df2ad71fd080719435036a3291c9c
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2023-12-21 15:34:14 -0300

    Adições no menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -22,11 +22,31 @@ export const Menu = (props) => {
         menuOpened ? "-rotate-45" : ""
      }`}
      />
-
      </buttom>
-    
+     <div
+     className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
+     ${menuOpened ? "w-80" : "w-0"}`}
+     >
+    <div className="flex-1 flex items-star justify-center flex-col gap-6 p-8">
+     <MenuButtom label="Sobre" onClick={() => onSectionChange(0)} />
+     <MenuButtom label="Skills" onClick={() => onSectionChange(1)} />
+     <MenuButtom label="Projetos" onClick={() => onSectionChange(2)} />
+     <MenuButtom label="Contato" onClick={() => onSectionChange(3)} />
 
-
+    </div>
+    </div>
     </>
-    )
-}
+    );
+};
+
+const MenuButtom = (props) => {
+    const { label, onClick } = props;
+    return (
+     <buttom
+        onClick={onClick}
+        className="text-2xl font-bold cursos-pointer hover:text-indigo-600 transition-color"
+    >
+        {label}
+    </buttom>
+    );
+};
