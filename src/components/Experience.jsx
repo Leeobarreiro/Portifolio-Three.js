e26bf3194bd18790da15feec56b2ea58f5601ea5
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2023-12-23 21:41:22 -0300

    Arquivos ajustados

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,17 +1,22 @@
 import { Office } from "./Office"
+import { motion } from "framer-motion"
 
-export const Experience = () => {
+export const Experience = (props) => {
+  const { section } = props
   return (
     <>
       <ambientLight intensity={1} />
-      <group
+      <motion.group
       position={[1.5, 2, 3]}
       scale={[0.9, 0.9, 0.9]}
       rotation-y={-Math.PI / 4}
+      animate={{
+        y: section === 0 ? 0 : -1
+      }}
 
       >
       <Office />
-      </group>
+      </motion.group>
     </>
   );
 };
