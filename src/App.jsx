bd2bf3827a2bd55f97307aab375748fa6b50fb8a
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2023-12-26 10:28:25 -0300

    ultimos ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
 import { Scroll, ScrollControls } from "@react-three/drei";
-import { Interface } from "./components/Interface";
-import { useEffect, useState } from "react";
-import { ScrollManager } from "./components/ScrollManager";
-import { Menu } from "./components/Menu";
+import { Canvas } from "@react-three/fiber";
 import { MotionConfig } from "framer-motion";
-
+import { Leva } from "leva";
+import { useEffect, useState } from "react";
+import { Cursor } from "./components/Cursor";
+import { Experience } from "./components/Experience";
+import { Interface } from "./components/Interface";
+import { Menu } from "./components/Menu";
+import { ScrollManager } from "./components/ScrollManager";
+import { framerMotionConfig } from "./config";
 
 function App() {
-  
-  const [section, setSection] = useState (0);
+  const [section, setSection] = useState(0);
   const [menuOpened, setMenuOpened] = useState(false);
 
   useEffect(() => {
@@ -19,34 +20,31 @@ function App() {
 
   return (
     <>
-    <MotionConfig
-    transition={{
-    type: "spring",
-    mass:5,
-    stiffness: 500,
-    damping: 50,
-    restDelta: 0.0001,
-
-  }}
-    >
-    <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
-      <color attach="background" args={["#e6e7ff"]} />
-      <ScrollControls pages={4} damping={0.1}>
-      <ScrollManager section={section} onSectionChange={setSection} />
-      <Scroll>
-      <Experience section ={section}/>
-      </Scroll>
-      <Scroll html>
-      <Interface />
-       </Scroll> 
-      </ScrollControls>
-    </Canvas>
-    <Menu 
-    onSectionChange={setSection}
-    menuOpened={menuOpened}  
-    setMenuOpened={setMenuOpened}
-    />
-    </MotionConfig>
+      <MotionConfig
+        transition={{
+          ...framerMotionConfig,
+        }}
+      >
+        <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+          <color attach="background" args={["#e6e7ff"]} />
+          <ScrollControls pages={4} damping={0.1}>
+            <ScrollManager section={section} onSectionChange={setSection} />
+            <Scroll>
+              <Experience section={section} menuOpened={menuOpened} />
+            </Scroll>
+            <Scroll html>
+              <Interface />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu
+          onSectionChange={setSection}
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+        />
+        <Cursor />
+      </MotionConfig>
+      <Leva hidden />
     </>
   );
 }
